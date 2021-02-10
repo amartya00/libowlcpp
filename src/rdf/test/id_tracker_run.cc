e920a9374a0172ca7f8ad0f47342957056b4ff75
Amartya Datta Gupta <amartya00@gmail.com>
2021-02-09 23:06:57 -0500

    [Refactor] Moved a couple of more rdf tests to catch cpp.

--- src/rdf/test/id_tracker_run.cc
+++ src/rdf/test/id_tracker_run.cc
@@ -3,26 +3,55 @@ part of %owlcpp project.
 @n @n Distributed under the Boost Software License, Version 1.0; see doc/license.txt.
 @n Copyright Mikhail K Levin 2011
 *******************************************************************************/
-#define BOOST_TEST_MODULE id_tracker_run
-#include "boost/test/unit_test.hpp"
+
+#define CATCH_CONFIG_MAIN
 
 #include "owlcpp/detail/id_tracker.hpp"
 #include "owlcpp/ns_id.hpp"
 
-namespace owlcpp{ namespace test{
+#include <catch2/catch.hpp>
+#include <iostream>
 
-/**@test
-*******************************************************************************/
-BOOST_AUTO_TEST_CASE( test_id_tracker_01 ) {
-   detail::Id_tracker<Ns_id> tracker( Ns_id(12) );
-   BOOST_CHECK_EQUAL(tracker.get(), Ns_id(13));
-   BOOST_CHECK_EQUAL(tracker.get(), Ns_id(14));
-   tracker.push(Ns_id(14));
-   tracker.push(Ns_id(13));
-   //tracker.push(Ns_id(15)); //assert violation
-   BOOST_CHECK_EQUAL(tracker.get(), Ns_id(13));
-   BOOST_CHECK_EQUAL(tracker.get(), Ns_id(14));
+using owlcpp::detail::Id_tracker;
+using owlcpp::Ns_id;
+
+SCENARIO("ID tracker test") {
+    
+    GIVEN("I have an ID tracker") {
+
+        Id_tracker<Ns_id> tracker {Ns_id {12}};
+        const Ns_id refId13 {13};
+        const Ns_id refId14 {14};
+        
+        WHEN("I extract more IDs") {
+
+            auto id13 {tracker.get()};
+            auto id14 {tracker.get()};
+
+            THEN("The IDs should be sequential") {
+                
+                REQUIRE(id13 == refId13);
+                REQUIRE(id14 == refId14);
+            }
+        }
+        
+        AND_WHEN("I push the last 2 IDs and re-get") {
+
+            tracker.get();
+            tracker.get();
+            
+            tracker.push(refId14);
+            tracker.push(refId13);
+            
+            auto id13 {tracker.get()};
+            auto id14 {tracker.get()};
+
+            THEN("I should get the same IDs on get again") {
+
+                REQUIRE(id13 == refId13);
+                REQUIRE(id14 == refId14);
+            }
+        }
+    }
 }
 
-}//namespace test
-}//namespace owlcpp
