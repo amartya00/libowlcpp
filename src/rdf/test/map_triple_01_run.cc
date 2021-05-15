b5a276795f531a84d19f27bb88ee91bae41972f1
Amartya Datta Gupta <amartya00@gmail.com>
2021-05-15 15:25:01 +0000

    [Refactor] Moved map triple test 1 to catch cpp.

--- src/rdf/test/map_triple_01_run.cc
+++ src/rdf/test/map_triple_01_run.cc
@@ -3,17 +3,25 @@ part of owlcpp project.
 @n @n Distributed under the Boost Software License, Version 1.0; see doc/license.txt.
 @n Copyright Mikhail K Levin 2012
 *******************************************************************************/
-#define BOOST_TEST_MODULE triple_map_01_run
-#include "boost/test/unit_test.hpp"
-#include "boost/range.hpp"
-#include "test/exception_fixture.hpp"
+#define CATCH_CONFIG_MAIN
+
+#include <catch2/catch.hpp>
 #include <iostream>
+#include <owlcpp/rdf/map_triple.hpp>
+#include <test_utils.hpp>
 
-#include "owlcpp/rdf/map_triple.hpp"
 namespace mpl = boost::mpl;
-#include "test_utils.hpp"
 
-namespace owlcpp{ namespace test{
+using owlcpp::Map_triple;
+using owlcpp::Subj_tag;
+using owlcpp::Pred_tag;
+using owlcpp::Obj_tag;
+using owlcpp::Doc_tag;
+using owlcpp::test::insert_seq;
+using owlcpp::any;
+using owlcpp::Node_id;
+using owlcpp::test::triple;
+using owlcpp::Rdf_err;
 
 const unsigned t[][4] = {
          {0,1,0,0},
@@ -25,66 +33,231 @@ const unsigned t[][4] = {
          {0,4,0,0},
 };
 
-/**@test
-*******************************************************************************/
-BOOST_AUTO_TEST_CASE( test_iterator ) {
-   typedef Map_triple<
+typedef Map_triple<
             mpl::vector1<
                mpl::vector4<Subj_tag, Pred_tag, Obj_tag, Doc_tag>
             >
    > map_triple;
-   map_triple mt;
-   BOOST_CHECK_EQUAL(mt.size(), 0U);
-   BOOST_CHECK(mt.begin() == mt.end());
-   insert_seq(mt, t);
-   BOOST_CHECK_EQUAL(mt.size(), 7U);
-   BOOST_CHECK(mt.begin() != mt.end());
 
+SCENARIO("Iterator test") {
+
+   GIVEN("I have a map triple") {
+
+      map_triple mt;
+
+      THEN("Its initial size should be 0") {
+
+         REQUIRE(mt.size() == 0U);         
+      }
+
+      WHEN("I insert the sample seq") {
+
+         insert_seq(mt, t);
+
+         THEN("The size should increase to 7") {
+
+            REQUIRE(mt.size() == 7U);
+            REQUIRE(mt.begin() != mt.end());            
+         }
+      }
+   }   
 }
 
-/**@test
-*******************************************************************************/
-BOOST_AUTO_TEST_CASE( test_index_subject_search ) {
-   typedef Map_triple<
-            mpl::vector1<
-               mpl::vector4<Subj_tag, Pred_tag, Obj_tag, Doc_tag>
-            >
-   > map_triple;
-   map_triple mt;
-   BOOST_CHECK_EQUAL(mt.size(), 0U);
-   insert_seq(mt, t);
-   BOOST_CHECK_EQUAL(mt.size(), 7U);
-   map_triple::query_b<1,1,0,0>::range r1 = mt.find(Node_id(2), Node_id(3), any, any);
-   BOOST_CHECK(!r1);
-   map_triple::query_b<1,1,0,0>::range r2 = mt.find(Node_id(0), Node_id(1), any, any);
-   BOOST_CHECK(r2);
-   BOOST_CHECK_EQUAL(boost::distance(r2), 1);
-   map_triple::query_b<0,1,0,0>::range r3 = mt.find(any, Node_id(3), any, any);
-   BOOST_CHECK_EQUAL(boost::distance(r3), 4);
 
-   BOOST_CHECK_THROW( mt.erase(triple(0,13,0,1)), Rdf_err );
-   mt.erase(triple(0,3,0,1));
-   BOOST_CHECK_EQUAL(mt.size(), 6U);
+SCENARIO("Test index subject search") {
 
-   mt.clear();
-   BOOST_CHECK_EQUAL(mt.size(), 0U);
+   GIVEN("I have a map triple") {
+
+      map_triple mt;
+
+      THEN("The initial size should be 0") {
+
+         REQUIRE(mt.size() == 0U);      
+      }
+
+      WHEN("I insert some elements") {
+
+         insert_seq(mt, t);
+
+         THEN("The new size should reflect that") {
+
+            REQUIRE(mt.size() == 7U);         
+         }
+
+         AND_WHEN("I search for some  on existant elements") {
+
+            auto r1 {mt.find(Node_id(2), Node_id(3), any, any)};
+
+            THEN("It should be not found") {
+
+               REQUIRE_FALSE(r1);            
+            }
+         }
+
+         AND_WHEN("I do look for something that exists") {
+
+            auto r2 {mt.find(Node_id(0), Node_id(1), any, any)};
+
+            THEN("It should exist") {
+
+               REQUIRE(r2);
+
+               AND_THEN("The distance should be 1") {
+
+                  REQUIRE(boost::distance(r2) == 1);
+               }
+            }
+         }
+
+         AND_WHEN("I search for another element further away") {
+
+            auto r3 {mt.find(any, Node_id(3), any, any)};
+
+            THEN("The distance should be appropriate") {
+
+               REQUIRE(boost::distance(r3) == 4);                           
+            }
+         }
+
+         AND_WHEN("I try to erase something that ain't there") {
+
+            THEN("I should get an error") {
+
+               REQUIRE_THROWS_AS(mt.erase(triple(0,13,0,1)), Rdf_err);
+            }
+         }
+
+         AND_WHEN("I do erase something that is there") {
+
+            mt.erase(triple(0,3,0,1));
+
+            THEN("I should be able to so so. The size reflects it.") {
+
+                REQUIRE(mt.size() == 6U);
+            }
+         }
+
+         AND_WHEN("WHEN I klear the thing") {
+
+            mt.clear();
+
+            THEN("Its size should go back to 0") {
+
+               REQUIRE(mt.size() == 0U);               
+            }
+         }
+      }
+   }
 }
 
-/**@test
-*******************************************************************************/
-BOOST_AUTO_TEST_CASE( test_index_predicate_search ) {
-   typedef Map_triple<
-            mpl::vector1<
-               mpl::vector4<Pred_tag, Subj_tag, Obj_tag, Doc_tag>
-            >
-   > map_triple;
-   map_triple mt;
-   insert_seq(mt, t);
-   typedef map_triple::query_b<0,1,0,0>::range range_t;
-   range_t r = mt.find(any, Node_id(3), any, any);
-   BOOST_CHECK(r);
-   BOOST_CHECK_EQUAL(distance(r), 4U);
+
+SCENARIO("Index subject search") {
+
+   GIVEN("I have a map triple") {
+
+      map_triple mt;
+
+      THEN("Its initial size should be 0") {
+
+         REQUIRE(mt.size() == 0U);  
+      }
+
+      AND_WHEN("I insert some elements in it") {
+
+         insert_seq(mt, t);
+
+         THEN("The size should reflect it") {
+
+            REQUIRE(mt.size() == 7U);
+         }
+
+         AND_WHEN("I search for non existent elements") {
+
+            auto r1 {mt.find(Node_id(2), Node_id(3), any, any)};
+   
+            THEN("I should get an not-found") {
+   
+               REQUIRE_FALSE(r1);
+            }
+         }
+   
+         AND_WHEN("I search for elements that do exist") {
+   
+            auto r2 = mt.find(Node_id(0), Node_id(1), any, any);
+   
+            THEN("I should find them") {
+   
+               REQUIRE(r2);            
+            }
+         }
+   
+         AND_WHEN("I compute distances of nodes") {
+   
+            auto r2 = mt.find(Node_id(0), Node_id(1), any, any);
+            auto r3 = mt.find(any, Node_id(3), any, any);
+   
+            THEN("I should get the proper distances") {
+   
+               REQUIRE(boost::distance(r2) == 1);
+               REQUIRE(boost::distance(r3) == 4);                       
+            }
+         }
+
+         AND_WHEN("I try to erase a non-existent element") {
+
+            THEN("I should get an error") {
+
+               REQUIRE_THROWS_AS(mt.erase(triple(0,13,0,1)), Rdf_err);
+            }
+         }
+
+         AND_WHEN("I try to erase an element that does exist") {
+
+            mt.erase(triple(0,3,0,1));
+
+            THEN("The size should reflect that") {
+
+               REQUIRE(mt.size() == 6U);
+            }
+         }
+
+         AND_WHEN("I clear the map") {
+
+            mt.clear();
+
+            THEN("Its size should go to 0") {
+
+               REQUIRE(mt.size() == 0U);  
+            }
+         }
+      }
+   }
 }
 
-}//namespace test
-}//namespace owlcpp
+SCENARIO("TEst index predicate search") {
+
+   GIVEN("I have a map triple") {
+
+      map_triple mt;
+
+      AND_GIVEN("I inserted elements into it") {
+
+         insert_seq(mt, t);
+
+         WHEN("I try to find an element in it") {
+
+            auto r {mt.find(any, Node_id(3), any, any)};
+
+            THEN("I should be able to do so") {
+
+               REQUIRE(r);
+            }
+
+            AND_THEN("I should be able to get the distance") {
+
+               REQUIRE(boost::distance(r) == 4U);
+            }
+         }
+      }
+   }
+}
